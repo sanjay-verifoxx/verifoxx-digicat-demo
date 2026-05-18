eb802a46382180065acc8e0791f183972457603f
sanjay-verifoxx <sanjay@verifoxx.com>
2026-05-18 16:56:09 +0100

    Update contact.js

--- js/screens/contact.js
+++ js/screens/contact.js
@@ -2,6 +2,7 @@ window.PETSDemoScreens = window.PETSDemoScreens || {};
 
 window.PETSDemoScreens.contact = (state, components) => `
   <section class="screen-frame">
+    ${components.renderTopbar({ title: "" })}
     <div class="screen-content contact-screen-content">
       <div class="contact-layout">
         <div class="contact-intro card rise-in">
